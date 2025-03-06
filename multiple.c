a11d6aba981d370ed408ab799c158434bf538cff
MRX-SKM <mithun.k@s.amity.edu>
2025-03-06 09:29:09 +0530

    first file

---
+++ multiple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int number, i;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d * %d = %d\n", number, i, number * i);
+    }
+    return 0;
+}
+
