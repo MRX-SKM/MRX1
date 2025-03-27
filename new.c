959f4ef2f29991bcb18de3c00db2ae994532f774
MRX-SKM <mithun.k@s.amity.edu>
2025-03-27 12:00:27 +0530

    added varibale in new.c

--- new.c
+++ new.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int *ptr;
-	int a;
-	ptr = &a;
-	printf("enter the vlaue of a:  );
-	scanf("%d",&a);
-	printf("entered value is %d\n",*ptr);
+	int *ptr1,*ptr2;
+	int a,b;
+	ptr1 = &a;
+	ptr2 = &b;
+	printf("enter the vlaue of a and b:  );
+	scanf("%d,%d",&a,&b);
+	printf("entered values are %d,%d\n",*ptr1,*ptr2);
 	return 0;
 }
